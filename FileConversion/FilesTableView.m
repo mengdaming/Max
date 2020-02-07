b9ad992741e3cd8bccc0fbe1cfa6675f152c112b
Damien Menanteau <damien.menanteau@gmail.com>
2020-02-07 19:59:06 +0100

    Allow for contextual menu in file conversion window to work with multiple selection

--- FileConversion/FilesTableView.m
+++ FileConversion/FilesTableView.m
@@ -28,107 +28,126 @@
 
 - (BOOL) validateMenuItem:(NSMenuItem *)menuItem
 {
-	if([menuItem action] == @selector(playWithPlay:))
-		return (nil != [[NSWorkspace sharedWorkspace] fullPathForApplication:@"Play"]);
-	else if([menuItem action] == @selector(editWithTag:))
-		return (nil != [[NSWorkspace sharedWorkspace] fullPathForApplication:@"Tag"]);
-	else
-		return YES;
+    if([menuItem action] == @selector(playWithPlay:))
+        return (nil != [[NSWorkspace sharedWorkspace] fullPathForApplication:@"Play"]);
+    else if([menuItem action] == @selector(editWithTag:))
+        return (nil != [[NSWorkspace sharedWorkspace] fullPathForApplication:@"Tag"]);
+    else
+        return YES;
 }
 
 - (NSDragOperation) draggingSourceOperationMaskForLocal:(BOOL)isLocal
 {
-	return (isLocal ? NSDragOperationMove : NSDragOperationCopy);
+    return (isLocal ? NSDragOperationMove : NSDragOperationCopy);
 }
 
 - (void) keyDown:(NSEvent *)event
 {
-	unichar			key		= [[event charactersIgnoringModifiers] characterAtIndex:0];    
-	unsigned int	flags	= [event modifierFlags] & 0x00FF;
+    unichar			key		= [[event charactersIgnoringModifiers] characterAtIndex:0];
+    unsigned int	flags	= [event modifierFlags] & 0x00FF;
     
-	if(NSDeleteCharacter == key && 0 == flags) {
-		if(-1 == [self selectedRow]) {
-			NSBeep();
-		}
-		else {
-			[_filesController removeObjectsAtArrangedObjectIndexes:[self selectedRowIndexes]];
-		}
-	}
-	else {
-		[super keyDown:event]; // let somebody else handle the event 
-	}
+    if(NSDeleteCharacter == key && 0 == flags) {
+        if(-1 == [self selectedRow]) {
+            NSBeep();
+        }
+        else {
+            [_filesController removeObjectsAtArrangedObjectIndexes:[self selectedRowIndexes]];
+        }
+    }
+    else {
+        [super keyDown:event]; // let somebody else handle the event
+    }
 }
 
 // TODO: provide prettier dragging images for files (larger icons ??)
 /*
-- (NSImage *) dragImageForRowsWithIndexes:(NSIndexSet *)dragRows tableColumns:(NSArray *)tableColumns event:(NSEvent*)dragEvent offset:(NSPointPointer)dragImageOffset
-{
-	return [super dragImageForRowsWithIndexes:dragRows tableColumns:tableColumns event:dragEvent offset:dragImageOffset];
-}
-*/
+ - (NSImage *) dragImageForRowsWithIndexes:(NSIndexSet *)dragRows tableColumns:(NSArray *)tableColumns event:(NSEvent*)dragEvent offset:(NSPointPointer)dragImageOffset
+ {
+ return [super dragImageForRowsWithIndexes:dragRows tableColumns:tableColumns event:dragEvent offset:dragImageOffset];
+ }
+ */
 
 - (NSMenu *) menuForEvent:(NSEvent *)event
 {
-	NSPoint		location		= [event locationInWindow];
-	NSPoint		localLocation	= [self convertPoint:location fromView:nil];
-	NSInteger	row				= [self rowAtPoint:localLocation];
-	
-	if(-1 != row) {
-		[self selectRowIndexes:[NSIndexSet indexSetWithIndex:row] byExtendingSelection:NO];
-		return [self menu];
-	}
-	
-	return nil;
+    NSPoint		location		= [event locationInWindow];
+    NSPoint		localLocation	= [self convertPoint:location fromView:nil];
+    NSInteger	row				= [self rowAtPoint:localLocation];
+    
+    if(-1 != row) {
+        [self selectRowIndexes:[NSIndexSet indexSetWithIndex:row] byExtendingSelection:YES];
+        return [self menu];
+    }
+    
+    return nil;
 }
 
 - (IBAction) openWithFinder:(id)sender
 {
-	NSString *path = [[_filesController selection] valueForKey:@"filename"];
-	[[NSWorkspace sharedWorkspace] openFile:path];
+    NSEnumerator *enumerator = [[_filesController selectedObjects] objectEnumerator];
+    NSDictionary *currentSelection;
+    
+    while((currentSelection = [enumerator nextObject])) {
+        NSString *path = [currentSelection valueForKey:@"filename"];
+        [[NSWorkspace sharedWorkspace] openFile:path];
+    }
 }
 
 - (IBAction) revealInFinder:(id)sender
 {
-	NSString *path = [[_filesController selection] valueForKey:@"filename"];
-	[[NSWorkspace sharedWorkspace] selectFile:path inFileViewerRootedAtPath:@""];
+    NSEnumerator *enumerator = [[_filesController selectedObjects] objectEnumerator];
+    NSDictionary *currentSelection;
+    
+    while((currentSelection = [enumerator nextObject])) {
+        NSString *path = [currentSelection valueForKey:@"filename"];
+        [[NSWorkspace sharedWorkspace] selectFile:path inFileViewerRootedAtPath:@""];
+    }
 }
 
 - (IBAction) playWithPlay:(id)sender
 {
-	NSString *path = [[_filesController selection] valueForKey:@"filename"];
-	[[NSWorkspace sharedWorkspace] openFile:path withApplication:@"Play"];
+    NSEnumerator *enumerator = [[_filesController selectedObjects] objectEnumerator];
+    NSDictionary *currentSelection;
+    
+    while((currentSelection = [enumerator nextObject])) {
+        NSString *path = [currentSelection valueForKey:@"filename"];
+        [[NSWorkspace sharedWorkspace] openFile:path withApplication:@"Play"];
+    }
 }
 
 - (IBAction) editWithTag:(id)sender
 {
-	NSString *path = [[_filesController selection] valueForKey:@"filename"];
-	[[NSWorkspace sharedWorkspace] openFile:path withApplication:@"Tag"];
+    NSEnumerator *enumerator = [[_filesController selectedObjects] objectEnumerator];
+    NSDictionary *currentSelection;
+    
+    while((currentSelection = [enumerator nextObject])) {
+        NSString *path = [currentSelection valueForKey:@"filename"];
+        [[NSWorkspace sharedWorkspace] openFile:path withApplication:@"Tag"];
+    }
 }
 
 - (IBAction) openWith:(id)sender
 {
-	NSOpenPanel		*panel		= [NSOpenPanel openPanel];
-	
-	[panel beginSheetForDirectory:nil file:nil types:[NSArray arrayWithObject:@"app"] modalForWindow:[self window] modalDelegate:self didEndSelector:@selector(openWithPanelDidEnd:returnCode:contextInfo:) contextInfo:NULL];	
-}
-
-@end
-
-@implementation FilesTableView (Private)
-
-- (void) openWithPanelDidEnd:(NSOpenPanel *)panel returnCode:(int)returnCode contextInfo:(void *)contextInfo
-{	
-	if(NSOKButton == returnCode) {
-		NSString			*path				= [[_filesController selection] valueForKey:@"filename"];
-		NSArray				*applications		= [panel filenames];
-		NSString			*applicationPath	= nil;
-		unsigned			i;
-		
-		for(i = 0; i < [applications count]; ++i) {
-			applicationPath = [applications objectAtIndex:i];
-			[[NSWorkspace sharedWorkspace] openFile:path withApplication:applicationPath];
-		}
-	}
-}
+    NSOpenPanel		*panel		= [NSOpenPanel openPanel];
+    
+    [panel beginSheetModalForWindow:[self window] completionHandler:^(NSModalResponse returnCode) {
+        if(NSModalResponseOK == returnCode) {
+            NSArray *applications = [panel URLs];
+            NSURL *applicationURL = nil;
+            unsigned i;
+            
+            for(i = 0; i < [applications count]; ++i) {
+                applicationURL = [applications objectAtIndex:i];
+                NSString *applicationPath = [applicationURL valueForKey:@"path"];
+                
+                NSEnumerator *enumerator = [[_filesController selectedObjects] objectEnumerator];;
+                NSDictionary *currentSelection;
+                while((currentSelection = [enumerator nextObject])) {
+                    NSString *path = [currentSelection valueForKey:@"filename"];
+                    [[NSWorkspace sharedWorkspace] openFile:path withApplication:applicationPath];
+                }
+            }
+        }
+    }];
+ }
 
 @end
